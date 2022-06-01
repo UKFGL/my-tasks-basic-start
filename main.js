cea40c71181decd411c984843974664f3f35beb7
UKFGL <opatel37@yahoo.com>
2022-06-01 10:08:53 -0600

    init updates

--- main.js
+++ main.js
@@ -12,11 +12,7 @@ let menuEl = document.getElementById("menu");
 let tasksEl = document.getElementById("tasks");
 
 // Global Variables
-let tasks = [
-  "Do the first thing",
-  "Do the second thing",
-  "oh yea, and the third thing",
-];
+let tasks = JSON.parse(localStorage.getItem("savedTasks")) ?? [];
 displayTasks();
 
 // Go Btn - Menu Listener
@@ -25,7 +21,7 @@ goBtnEl.addEventListener("click", goBtnHandler);
 function goBtnHandler() {
   // Get Menu Selection
   let selection = menuEl.value;
-
+  
   if (selection === "add") {
     addTask();
   } else if (selection === "remove") {
@@ -37,20 +33,52 @@ function goBtnHandler() {
 
 function displayTasks() {
   // Diplay all tasks by putting them in the 'tasksEl' <div>
+  let tasksStr = "";
+
+  for (let i = 0; i < tasks.length; i++) {
+    tasksStr += "<li>" + tasks[i] + "</li>"
+  }
+
+  tasksEl.innerHTML = tasksStr;
 }
 
 function addTask() {
   // Prompt user for a new task
+  let newTask = prompt("What's your next task?");
+
   // Add task to tasks array
+  tasks.push(newTask);
+  saveTasks();
+
   // Display all tasks to show changes
+  displayTasks();
+
 }
 
 function removeTask() {
   // Prompt user for task to remove
+  let choosenTask = prompt("Enter number of task you wish to remove");
+
   // Remove task from task array (if it exists)
+  tasks.splice(choosenTask - 1, 1);
+  saveTasks();
+
   // Display all tasks to show changes
+  displayTasks();
 }
 
 function clearAll() {
   // Clear all tasks
+  let confirmation = prompt("Are you sure you want to clear your list? Enter 'yes' & press ok to clear list.");
+
+  if (confirmation === "yes") {
+    tasks = [];
+  } 
+
+  displayTasks();
+}
+
+function saveTasks() {
+  // Save tasks to localStorage
+  localStorage.setItem("savedTasks", JSON.stringify(tasks));
 }
